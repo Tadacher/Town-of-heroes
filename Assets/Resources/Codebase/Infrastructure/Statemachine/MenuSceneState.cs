c195cfaff3e3f8c0b621eb3abffae7c8e34cb082
SergeyGnedkov <sergeygnedkovvv@gmail.com>
2024-12-12 00:26:36 +0400

    minor fix

---
+++ Assets/Resources/Codebase/Infrastructure/Statemachine/MenuSceneState.cs
@@ -0,0 +1,20 @@
+﻿namespace Infrastructure
+{
+    public class MenuSceneState : AbstractSceneState
+    {
+        public MenuSceneState(string sceneName) : base(sceneName)
+        {
+
+        }
+
+        public override void Enter()
+        {
+
+        }
+
+        public override void Exit()
+        {
+
+        }
+    }
+}
