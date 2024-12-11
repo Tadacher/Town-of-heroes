669b84febbe3f26f2997e2d8b429f627c2d4684f
Daniil Levin <dan3000pro@mail.ru>
2024-12-11 20:57:19 +0400

    minor dix

--- Assets/Resources/Codebase/Enemies/HealthModules/PassiveDefensiveAbilities/AbstractPassiveDefensiveAbility.cs
+++ Assets/Resources/Codebase/Enemies/HealthModules/PassiveDefensiveAbilities/AbstractPassiveDefensiveAbility.cs
@@ -4,6 +4,12 @@
     /// must be no abilities with same priority except 0
     /// </summary>
     public int Priority;
+
+
     public abstract float ProcessDamage(float damage);
+    protected AbstractPassiveDefensiveAbility(int priority)
+    {
+        Priority = priority;
+    }
 }
 
